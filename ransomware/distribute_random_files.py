4fef54ca9bebe93429e569479361123ca6ce9b23
Chris <chall168421@gmail.com>
2022-06-12 10:33:21 +0700

    code reformatting

--- ransomware/distribute_random_files.py
+++ ransomware/distribute_random_files.py
@@ -2,86 +2,76 @@ import os
 from random import randint, sample, choice
 from shutil import copy
 
+## helper script to copy a ton of random files to use for the activity
 
 
 path = "C:\\Users\\chall\\my drive\\Python\\security_activity_test\\ransomware\\random_files"
 
-wanted = ['docx', 'txt', 'pdf','jpg']
-
+wanted = ['docx', 'txt', 'pdf', 'jpg']
 
 
 def distribute(new_path):
+	print("Creating files for", new_path)
 
-    print("Creating files for", new_path)
+	file_stubs = "document,my,story,song,listing,information,download,doc,today,untitled,unnamed,research,homework,info,doc,letter,email,info".split(
+		",")
 
-    file_stubs = "document,my,story,song,listing,information,download,doc,today,untitled,unnamed,research,homework,info,doc,letter,email,info".split(",")
+	for ftype in wanted:
 
-    for ftype in wanted:
+		this_path = path
 
-        this_path = path
+		if ftype == "jpg":
 
-        if ftype == "jpg":
+			sub = choice('IMG,images,pics,photo,pictures,image_folder'.split(","))
+			new_path += f"\\{sub}"
 
-            sub = choice('IMG,images,pics,photo,pictures,image_folder'.split(","))
-            new_path += f"\\{sub}"
+			if os.path.exists(new_path) == False:
+				os.mkdir(new_path)
 
-            if os.path.exists(new_path) == False:
-                os.mkdir(new_path)
+			file_stubs = "pic,img,IMG,this,photo,graphic,gfx,pic,download".split(",")
 
-            file_stubs = "pic,img,IMG,this,photo,graphic,gfx,pic,download".split(",")
+		files = [f for f in os.listdir(path) if f.endswith(ftype)]
 
-        
+		needed = randint(2, int(len(files) * 0.4))
 
-        
+		files = sample(files, needed)
 
-        files = [f for f in os.listdir(path) if f.endswith(ftype)]
+		for fn in files:
 
+			new_fn = choice(file_stubs)
 
-        needed = randint(2, int(len(files)*0.4))
-        
-        files = sample(files, needed)
+			x = randint(0, 3)
+			if x == 0:
+				new_fn = new_fn.upper()
+			elif x == 1:
+				new_fn = new_fn
+			else:
+				new_fn = new_fn.title()
 
-        for fn in files:
+			x = randint(0, 3)
+			if x > 0:
+				if randint(0, 2) == 0:
+					num = randint(1, 999)
+				else:
+					num = randint(1, 99)
 
-            new_fn = choice(file_stubs)
+				num = str(num)
 
-            x = randint(0, 3)
-            if x == 0:
-                new_fn = new_fn.upper()
-            elif x == 1:
-                new_fn = new_fn
-            else:
-                new_fn = new_fn.title()
+				if randint(0, 3) == 0:
+					num = num.ljust(randint(1, 5), choice("A0_1-"))
 
-            x = randint(0, 3)
-            if x > 0:
-                if randint(0, 2) == 0:
-                    num = randint(1, 999)
-                else:
-                    num = randint(1, 99)
+				if randint(0, 1):
+					num = choice("_- ") + num
 
-                num = str(num)
+			else:
+				num = ""
 
-                if randint(0, 3) == 0:
+			new_fn = new_fn + num + "." + ftype.upper()
 
-
-                    num = num.ljust(randint(1, 5), choice("A0_1-"))
-
-                if randint(0, 1):
-                    num = choice("_- ") + num
-
-            else:
-                num = ""
-                
-            new_fn = new_fn + num + "." + ftype.upper()
-            
-            print("will copy", path+"\\"+fn, "to", new_path+"\\"+new_fn)
-            copy(path+"\\"+fn, new_path+"\\"+new_fn)
-    
-        
+			print("will copy", path + "\\" + fn, "to", new_path + "\\" + new_fn)
+			copy(path + "\\" + fn, new_path + "\\" + new_fn)
 
 
-
+## distribute files to sandbox network drives
 for num in range(1, 23):
-    
-    distribute(f"Q:\\Exam\\y8student{num}")
+	distribute(f"Q:\\Exam\\y8student{num}")
--- ransomware/fake_ransom/conv_file_to_hex.py
+++ ransomware/fake_ransom/conv_file_to_hex.py
@@ -1,67 +1,60 @@
-
 from os import listdir
 
 
+# initial script to write fake ransom files to binary
+
 def files_to_hex():
+	for f in listdir():
 
-    for f in listdir():
+		if f.endswith("py") or f.endswith("hex"):
+			continue
 
-        if f.endswith("py") or f.endswith("hex"):
-            continue
+		with open(f, "rb") as ransom:
+			fn, ext = f.split(".")
+			bina = "".join([bin(b)[2:].zfill(8) for b in ransom.read()])
+			den = int(bina, 2)
 
-        with open(f, "rb") as ransom:
-            fn, ext = f.split(".")
-            bina = "".join([bin(b)[2:].zfill(8) for b in ransom.read()])
-            den = int(bina, 2)
-           
-            with open(ext+".hex", "w") as hexfile:
+			with open(ext + ".hex", "w") as hexfile:
+				hexfile.write(hex(den)[2:])
 
-                hexfile.write(hex(den)[2:])
+				print("converted", ext)
 
-                print("converted", ext)     
 
 def hex_to_files():
+	for f in listdir():
 
-    for f in listdir():
+		if f.endswith(".hex"):
 
-        if f.endswith(".hex"):
+			with open(f) as hexfile:
+				hex_data = hexfile.read().strip()
 
-            with open(f) as hexfile:
-                hex_data = hexfile.read().strip()
+			ext, _ = f.split(".")
 
-            ext, _ = f.split(".")
+			with open("test_reload." + ext, "wb") as bin_file:
 
-     
-            
-            with open("test_reload."+ext, "wb") as bin_file:
+				byte_data = bytearray()
+				byte = ""
+				for nibble in hex_data:
+					byte += bin(int(nibble, 16))[2:].zfill(4)
 
-                byte_data = bytearray()
-                byte = ""
-                for nibble in hex_data:
-                    byte += bin(int(nibble, 16))[2:].zfill(4)
-                    
-                    if len(byte) == 8:
-                        this_byte = int(byte, 2)
-                        byte_data.append(this_byte)
-                                            
-                        byte = ""
+					if len(byte) == 8:
+						this_byte = int(byte, 2)
+						byte_data.append(this_byte)
 
+						byte = ""
 
+				bin_file.write(byte_data)
 
+				print("finished with " + ext)
 
-
-                bin_file.write(byte_data)
-
-                print("finished with " + ext)
-                    
 
 if __name__ == "__main__":
 
-    x = input('''
+	x = input('''
 1. to convert files to hex
 2. to convert hex to files
 ''')
-    if x == '1':
-        files_to_hex()
-    elif x == '2':
-        hex_to_files()
+	if x == '1':
+		files_to_hex()
+	elif x == '2':
+		hex_to_files()
